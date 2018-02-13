9951b52a8494713f742bcc4629850d0b49248d96
Dylan P Wong <dpwong@timberlake.cse.buffalo.edu>
2018-02-12 23:19:56 -0500

    Basic skeleton for pre

--- pre_proj.c
+++ pre_proj.c
@@ -1,9 +1,69 @@
 #include<stdio.h>
+#include<string.h>// lets us use the strcmp function. Used o compare strings in C
 /*Joe Mingoia
  *Tyler Turnbull
- *Douglas Achkar */
+ *Douglas Achkar
+ .Dylan Wong */
 
 
-int main(){
+int main(int argc, char* argv[]){
+
+FILE* inFile = NULL;  // To open the csv file
+
+char _f[]="-f";	char _r[]="-r";	char _h[]="-h";
+
+char max[] = "max";
+
+char min[] = "min";
+
+char mean[]= "mean";
+
+int i =1;// for loop iterator vairable. Its 1 cause argv[0] is this program
+
+for(i=1;i<=argc;i++)
+{
+
+         if(strcmp(argv[i],_f)==0 ) // compares both strings to see if they match
+		{
+		// Display the number of fields. To do this I think we count the number of comas and add 1
+		} 
+	else if(strcmp(argv[i],_r)==0)
+		{
+		//Display the number of records in file. We could count the amount of times we scan
+		}
+	else if(strcmp(argv[i],_h)==0)
+		{
+		//Treat the first record of the file as a header record rather than a data record
+		//This is kind of painful, but we could make an int that represents true and false,1 and 0. and have an if statement everywhere that would
+		//progress the first scan without doing anything.
+		}          
+	
+
+	
+	else if(strcmp(argv[i],max)==0) // Compares strings
+		{
+		//Displays the max value in the indicated field
+		// we could scan the whole field and just have a vairable that keeps track of the highest number. 
+		}
+	
+		
+	else if(strcmp(argv[i],min)==0)
+		{
+		//Displays the lowest value in the indicated field
+		// Same as Max but for min
+		}
+
+	
+	else if(strcmp(argv[i],mean)==0)
+		{
+		// Displays the mean in a given field. 
+		// Can have a variable that sums everything up and a counter to count how many times we scanned. 
+		}
+	else if (5==5) // ref field value goes here. I dont know what the argument call is 
+		{
+
+		}
+}
+  fclose(inFile);
   return 0;
 }
